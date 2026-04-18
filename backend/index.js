caa1a94c2519f95b739dfd1fa47fcc4541085559
divykasodariya <kasodariyadivy7@google.com>
2026-04-18 11:02:56 +0530

    db connnected

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/database.js';
-import userRoute from './routes/userRoute.js'
-import messageRoute from './routes/messageRoute.js'
+
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
-import { app, io, server } from './socket/socket.js';
+import http from 'http'
 dotenv.config({});
 
 
 const PORT = process.env.PORT || 6969;
 
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
     origin: [
         'http://localhost:3000',
-        'http://localhost:5173',  // Vite dev server
-        'https://pingy-chat-app.vercel.app',
-        "https://pingy-chat-app-git-main-divykasodariyas-projects.vercel.app",  // Your Vercel URL
-        'https://pingy-chatapp.on.shiper.app'  // Your Shipper backend URL (if needed)
+        'http://localhost:5173'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -28,17 +25,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-// Socket.IO connection handling
-io.on('connection', (socket) => {
-    console.log("user connected", socket.id);
-    socket.on("disconnect", () => {
-        console.log("Client disconnected", socket.id);
-    });
-});
 
-// routes
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/message", messageRoute);
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
     connectDB();
